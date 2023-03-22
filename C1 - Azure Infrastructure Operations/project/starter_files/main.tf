261917ec1ab48900e4631200cba9d1f757dbea58
Abdulrazak.Alahmad <abdulrazak.alahmad@shell.com>
2023-03-23 00:32:23 +0100

    add the files to the repo

---
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -0,0 +1,83 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/22"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-public-IP"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+
+  tags = var.tags
+}
+
+resource "azurerm_lb" "main" {
+  name                = "${var.prefix}-Load-Balancer"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "BackEndAddressPool"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  count                           = var.vm_count # Count Value read from variable
+  name                            = "${var.prefix}-vm-${count.index}"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_D2s_v3"
+  admin_username                  = "${var.username}"
+  admin_password                  = "${var.password}"
+  tags                            = var.tags
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+  source_image_id = data.azurerm_image.image.id
+  availability_set_id = azurerm_availability_set.main.id
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
---
+++ C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
@@ -0,0 +1,29 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+   default = "Azuredevops"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "WEST Europe"
+}
+
+variable "username" {
+  description = "enter for defalt username"
+  default = "abdulrazak_alahmad"
+}
+
+variable "password" {
+  description = "enter for defalt password"
+  default = "Abed!1234567"
+}
+
+variable "tags" {
+  description = "Vms tags"
+  default = "udacity"
+}
+
+variable "count" {
+  description = "Number of Virtual Machines"
+  default = 3
+}
